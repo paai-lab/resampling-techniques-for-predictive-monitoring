b4d8a136a157e81c27e910c007fdd87df100c5fc
jckim9203 <jckim9203@gmail.com>
2020-12-02 12:29:28 +0900

    Update production_train_test.py

--- production_train_test.py
+++ production_train_test.py
@@ -17,7 +17,7 @@ import sys
 from sys import argv
 import six
 sys.modules['sklearn.externals.six'] = six
-from resampling.resampler import resampling_assigner
+from resampling.resampler_production import resampling_assigner
 from imblearn.metrics import geometric_mean_score
 from collections import Counter
 
